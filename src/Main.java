c335fdb979dd51bdd559c7d3a06a8c2004a4db3e
Pavithra <v.pavithrav1011@gmail.com>
2024-07-10 11:16:00 +0530

    Convert character of a string into binary form

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String s;
+
+        System.out.println("Input String : ");
+        s = sc.nextLine();
+
+        System.out.println("\nThe result : ");
+        for(int i=0; i<s.length(); i++)
+        {
+            char ch  = s.charAt(i);
+            int asciiVal = (int) ch;
+            String binaryString = Integer.toBinaryString(asciiVal);
+            String formattedBinary = String.format("%7s", binaryString).replace(' ', '0');
+            System.out.printf("%c : %s \n",ch, formattedBinary);
+        }
+        System.out.println();
+    }
+}
